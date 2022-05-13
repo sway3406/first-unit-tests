7e25458641ebe6b2c9bb8837d9f0075e429baf5b
Shane Way <shane.c.way2@gmail.com>
2022-05-12 19:43:32 -0600

    Inital commit

--- functions.js
+++ functions.js
@@ -1,9 +1,36 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
+
+// sayHello fxn goes here.
+
+function sayHello(name) {
+  if (name === 'Will') {
+    return `No more testing ${name}!`;
+  } else {
+    return `Hi there ${name}!`;
+  }
+}
+
+
+// buildCar fxn goes here
+
+ function buildCar(shade, model) {
+  if(shade === undefined && model === undefined) {
+    return {};
+}
+    return {
+      color: shade,
+      type: model,
+    }
+  }
+
+
+
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
